b681a16b6e1c9c38a31faba82f76ea1c17a310a5
Cameron Dutro <camertron@gmail.com>
2016-05-16 11:41:11 -0700

    Add a few more test cases to the hook handler spec

--- spec/handlers/hook_handler_spec.rb
+++ spec/handlers/hook_handler_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 require 'spec_helpers/test_backend'
 require 'spec_helpers/test_db'
-require 'spec_helpers/test_request'
 require 'uri'
 
 include Txdb::Handlers
@@ -27,20 +26,45 @@ describe HookHandler do
     )
   end
 
-  it 'downloads and writes new content to the database' do
-    content = { 'phrase' => 'trans' }
-    expect(project.api).to receive(:download).and_return(YAML.dump(content))
+  it "doesn't write content when unauthorized" do
     params = { 'resource' => table.resource.resource_slug, 'language' => 'es' }
     body = URI.encode_www_form(params.to_a)
-    headers = { Txgh::TransifexRequestAuth::RACK_HEADER => sign(body) }
-
-    post '/transifex', body, headers
+    post '/transifex', body
 
     expect(last_response.status).to eq(200)
     expect(last_response.body).to eq('{}')
 
-    expect(Txdb::TestBackend.writes).to include(
-      locale: 'es', table: table.name, content: content
-    )
+    expect(Txdb::TestBackend.writes).to be_empty
+  end
+
+  context 'with an authorized request' do
+    let(:content) { { 'phrase' => 'trans' } }
+    let(:params) { { 'resource' => table.resource.resource_slug, 'language' => 'es' } }
+    let(:body) { URI.encode_www_form(params.to_a) }
+    let(:headers) { { Txgh::TransifexRequestAuth::RACK_HEADER => sign(body) } }
+
+    it 'downloads and writes new content to the database' do
+      expect(project.api).to receive(:download).and_return(YAML.dump(content))
+
+      post '/transifex', body, headers
+
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to eq('{}')
+
+      expect(Txdb::TestBackend.writes).to include(
+        locale: 'es', table: table.name, content: content
+      )
+    end
+
+    it 'reports errors' do
+      expect(project.api).to receive(:download).and_raise('jelly beans')
+
+      post '/transifex', body, headers
+
+      expect(last_response.status).to eq(500)
+      expect(JSON.parse(last_response.body)).to eq(
+        [{ 'error' => 'Internal server error: jelly beans' }]
+      )
+    end
   end
 end
