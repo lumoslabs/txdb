c458688db64537888d0e3b6e45ca8c2516699e28
Cameron Dutro <camertron@gmail.com>
2016-05-26 14:23:38 -0700

    Add dates to the created_at and updated_at columns if they exist

--- lib/txdb/backends/globalize/writer.rb
+++ lib/txdb/backends/globalize/writer.rb
@@ -29,6 +29,43 @@ module Txdb
 
         private
 
+        def update_row(id, fields, locale)
+          row = table.db.where(foreign_key.to_sym => id, locale: locale)
+
+          if row.empty?
+            table.db << fields
+              .merge(foreign_key.to_sym => id, locale: locale)
+              .merge(created_at)
+              .merge(updated_at)
+          else
+            row.update(fields.merge(updated_at))
+          end
+        end
+
+        def created_at
+          if table.db.columns.include?(:created_at)
+            { created_at: get_utc_time }
+          else
+            {}
+          end
+        end
+
+        def updated_at
+          if table.db.columns.include?(:updated_at)
+            { updated_at: get_utc_time }
+          else
+            {}
+          end
+        end
+
+        def get_utc_time
+          Time.now.utc
+        end
+
+        def deserialize_content(content)
+          YAML.load(content)
+        end
+
         def foreign_key
           @foreign_key ||= table.name.sub(/_translations/, '_id')
         end
